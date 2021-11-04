839e8c35dbd5b2a6b5237e4dfb9bfdb71092b65b
Eliott <Eliott.Jouneau@USherbrooke.ca>
2021-11-04 15:56:06 +0100

    aggTrad

--- base_websocket.js
+++ base_websocket.js
@@ -1,9 +1,13 @@
 'use strict'
-const { Spot } = require('@binance/connector')
 
-const client = new Spot('', '', {
+const api_key = require("./apikey.json")
+const { Spot } = require('@binance/connector')
+const { default: Binance } = require("node-binance-api")
+
+const client = new Spot(api_key.public_key, api_key.secret_key, {
   wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
 })
+
 
 const callbacks = {
   open: () => client.logger.log('open'),
@@ -13,7 +17,7 @@ const callbacks = {
 const aggTrade = client.aggTradeWS('bnbusdt', callbacks)
 
 // unsubscribe the stream above
-setTimeout(() => client.unsubscribe(aggTrade), 3000)
+//setTimeout(() => client.unsubscribe(aggTrade), 3000)
 
 // support combined stream
-const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@tikcer'], callbacks)
+//const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@tikcer'], callbacks)
