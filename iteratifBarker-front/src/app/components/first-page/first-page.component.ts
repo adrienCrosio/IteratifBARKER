08160283957c494a148f0248013bd5c23cc3ae3c
Adrien Crosio <adrien.crosio@domwee.com>
2021-11-17 18:52:51 +0100

    working dynamic chart

--- iteratifBarker-front/src/app/components/first-page/first-page.component.ts
+++ iteratifBarker-front/src/app/components/first-page/first-page.component.ts
@@ -1,27 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
   styleUrls: ['./first-page.component.css']
 })
-export class FirstPageComponent implements OnInit {
+export class FirstPageComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(BaseChartDirective, { static: false }) chart!: BaseChartDirective;
 
   constructor() { }
-  barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
+
+  barChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
+      }
+    }
   };
-  barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  barChartType: ChartType = 'bar';
-  barChartLegend = true;
-  barChartData = [
+  barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  barChartType: ChartType = "line";
+  barChartLegend: boolean = true;
+  barChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
   ];
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.dynamicArrayHandle();
+  }
+
+  async dynamicArrayHandle() {
+    while (true) {
+      let max_cap_reached = false;
+      if (this.barChartLabels.length > 10) {
+        max_cap_reached = true;
+        this.barChartLabels.shift();
+      }
+      for (const data of this.barChartData) {
+        let randomNumber = Math.floor(Math.random() * 100);
+        data.data?.push(randomNumber);
+        if(max_cap_reached){
+          data.data?.shift();
+        }
+      }
+      let prevLabel = parseInt('' + this.barChartLabels[this.barChartLabels.length - 1]);
+      this.barChartLabels.push((prevLabel + 1).toString());
+      this.chart.update();
+      await this.sleep(1500);
+    }
+  }
+
+  async sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
