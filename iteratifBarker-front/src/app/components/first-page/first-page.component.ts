abc8225572fa029874a7c44335e694ad2c6b4371
Adrien Crosio <adrien.crosio@domwee.com>
2021-11-17 13:36:36 +0100

    working first page with chart

--- iteratifBarker-front/src/app/components/first-page/first-page.component.ts
+++ iteratifBarker-front/src/app/components/first-page/first-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-first-page',
@@ -8,6 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class FirstPageComponent implements OnInit {
 
   constructor() { }
+  barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ];
 
   ngOnInit(): void {
   }
